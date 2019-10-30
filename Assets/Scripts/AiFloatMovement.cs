4212c1b8b2e282d857e0b42203ce7928f91ce9ab
Nicha <NichaRoj@gmail.com>
2019-10-31 00:33:00 +0200

    Make AI eye flip when moving

---
+++ Assets/Scripts/AiFloatMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiFloatMovement : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public GameObject mainCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDiff = mainCharacter.transform.position.x - transform.position.x;
+        if (xDiff > 0)
+        {
+            spriteRenderer.flipX = true;
+        } else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
