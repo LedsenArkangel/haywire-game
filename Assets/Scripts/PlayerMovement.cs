d9eef524e5690fcf342658152fad0fdc248e8eeb
Nicha <NichaRoj@gmail.com>
2019-12-05 01:17:57 +0200

    Pause player with first dialog

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 7;
     public float jump = 100;
     public float minJump = 10;
+    public Fungus.Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,52 +27,56 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") < 0) {
-            // left
-            spriteRenderer.flipX = true;
-        } else if (Input.GetAxis("Horizontal") > 0) {
-            // right
-            spriteRenderer.flipX = false;
-        }
+        if (!flowchart.GetBooleanVariable("isPaused")) {
+            if (Input.GetAxis("Horizontal") < 0) {
+                // left
+                spriteRenderer.flipX = true;
+            } else if (Input.GetAxis("Horizontal") > 0) {
+                // right
+                spriteRenderer.flipX = false;
+            }
 
-        if (Input.GetAxis("Jump") != 0 && !animator.GetBool("isClimbing")) {
-            // interact
-            StartCoroutine(PlayInteracting());
+            if (Input.GetAxis("Jump") != 0 && !animator.GetBool("isClimbing")) {
+                // interact
+                StartCoroutine(PlayInteracting());
+            }
         }
     }
 
     // Update is called at constant rate independent of frame rate
     void FixedUpdate()
     {
-        if (animator.GetBool("isClimbing")) {
-            // Climb
-            if (Input.GetAxis("Horizontal") != 0) {
-                // Jump out
-                animator.SetFloat("climbingSpeed", 1.0F);
-                animator.SetBool("isClimbing", false);
-                rigidbody.gravityScale = 1;
-            }
-            
-            if (Input.GetAxis("Vertical") != 0) {
-                var verticalSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-                transform.position += new Vector3(0, verticalSpeed, 0);
-                animator.SetFloat("climbingSpeed", 1.0F);
+        if (!flowchart.GetBooleanVariable("isPaused")) {
+            if (animator.GetBool("isClimbing")) {
+                // Climb
+                if (Input.GetAxis("Horizontal") != 0) {
+                    // Jump out
+                    animator.SetFloat("climbingSpeed", 1.0F);
+                    animator.SetBool("isClimbing", false);
+                    rigidbody.gravityScale = 1;
+                }
+                
+                if (Input.GetAxis("Vertical") != 0) {
+                    var verticalSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+                    transform.position += new Vector3(0, verticalSpeed, 0);
+                    animator.SetFloat("climbingSpeed", 1.0F);
+                } else {
+                    animator.SetFloat("climbingSpeed", 0.0F);
+                }
+                
             } else {
-                animator.SetFloat("climbingSpeed", 0.0F);
-            }
-            
-        } else {
-            // Horizontal movement
-            var horizontalSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            transform.position += new Vector3(horizontalSpeed, 0, 0);
-            animator.SetFloat("horizontal", Mathf.Abs(horizontalSpeed));
+                // Horizontal movement
+                var horizontalSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+                transform.position += new Vector3(horizontalSpeed, 0, 0);
+                animator.SetFloat("horizontal", Mathf.Abs(horizontalSpeed));
 
-            // Jump
-            var verticalSpeed = rigidbody.velocity.y;
-            animator.SetFloat("vertical", verticalSpeed);
-            if (Input.GetAxis("Vertical") > 0 && verticalSpeed == 0) {
-                var jumpForce = Input.GetAxis("Vertical") * jump * minJump;
-                rigidbody.AddForce(new Vector2(0, Mathf.Min(jump, jumpForce)), ForceMode2D.Impulse);
+                // Jump
+                var verticalSpeed = rigidbody.velocity.y;
+                animator.SetFloat("vertical", verticalSpeed);
+                if (Input.GetAxis("Vertical") > 0 && verticalSpeed == 0) {
+                    var jumpForce = Input.GetAxis("Vertical") * jump * minJump;
+                    rigidbody.AddForce(new Vector2(0, Mathf.Min(jump, jumpForce)), ForceMode2D.Impulse);
+                }
             }
         }
     }
@@ -90,16 +95,18 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void OnTriggerStay2D(Collider2D other) {
-        if (other.tag == "Ladder" && Input.GetAxis("Vertical") != 0) {
-            animator.SetBool("isClimbing", true);
-            // Snap to ladder
-            transform.position = new Vector2(other.gameObject.transform.position.x, transform.position.y);
-            // Stop gravity
-            rigidbody.velocity = new Vector2(0, 0);
-            rigidbody.gravityScale = 0;
-            // Use climbing animation
-            animator.Play("climbing");
-            animator.SetFloat("climbingSpeed", 1.0F);
+        if (!flowchart.GetBooleanVariable("isPaused")) {
+            if (other.tag == "Ladder" && Input.GetAxis("Vertical") != 0) {
+                animator.SetBool("isClimbing", true);
+                // Snap to ladder
+                transform.position = new Vector2(other.gameObject.transform.position.x, transform.position.y);
+                // Stop gravity
+                rigidbody.velocity = new Vector2(0, 0);
+                rigidbody.gravityScale = 0;
+                // Use climbing animation
+                animator.Play("climbing");
+                animator.SetFloat("climbingSpeed", 1.0F);
+            }
         }
     }
 }
