594618057d7be16a7a16c682c2a1708e4b6542d3
Nicha <NichaRoj@gmail.com>
2019-12-04 22:23:18 +0200

    Implement destroyables and check

---
+++ Assets/Scripts/DestroyableCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableCheck : MonoBehaviour
+{
+    public List<DestroyableBehavior> destroyables;
+    public string messageToFlowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if all is destroyed
+        if (!(destroyables.Exists(d => !d.isDestroyed))) {
+            Fungus.Flowchart.BroadcastFungusMessage(messageToFlowchart);
+        }
+    }
+}
